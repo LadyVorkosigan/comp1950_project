7693fff9ca965fd57f32395838da83105242be25
Jalehs <45085673+Jalehs@users.noreply.github.com>
2018-11-27 23:14:33 -0800

    Add files via upload

--- comp1950_project-master-php/includes/header.php
+++ comp1950_project-master-php/includes/header.php
@@ -1,12 +1,12 @@
 <header class="container-fluid">
-            <a href="http://bcitcomp.ca/">
-                <img src="images/bcit_logo_med.jpg" alt="BCIT logo"></a>
-            <span class="pad"> <h1>COMP1950 Thursday Evening Class</h1></span>
-            <?php
-                date_default_timezone_set("Canada/Pacific");
-    
-                $today = date("M. j,Y");	
-                echo "<p style='padding:0 0 5px 420px; font-size:14px;' >$today</p>";
-            ?>
+    <a href="http://bcitcomp.ca/">
+        <img src="images/bcit_logo_med.jpg" alt="BCIT logo"></a>
+    <span class="pad"> <h1>COMP1950 Thursday Evening Class</h1></span>
+    <span>
+    <?php
+        date_default_timezone_set("Canada/Pacific");
+        echo '<div style="margin:0 0 100px 440px">'.date("M. j, Y").'</div>';
+    ?>
+    </span>
             
  </header>
